c1d4398b60a5a28fe60d23806bab51afca7bbe1b
hugovntr <26794193+hugovntr@users.noreply.github.com>
2024-05-26 11:07:58 +0200

    feat(navigation): add flash.nvim plugin for enhanced navigation
    Added flash.nvim plugin to improve navigation. Configured for lazy
    loading and set up keybinding for Ctrl+F to invoke flash jump.

---
+++ .forge/nvim/lua/plugins/navigation.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    ---@type Flash.Config
+    opts = {},
+    keys = {
+      {
+        '<c-f>',
+        mode = { 'n', 'x', 'o' },
+        function()
+          require('flash').jump()
+        end,
+        desc = 'Flash',
+      },
+    },
+  },
+}
