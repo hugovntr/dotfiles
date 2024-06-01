7b638478d7a5a343513090aa71f161e2525c67bd
hugovntr <26794193+hugovntr@users.noreply.github.com>
2024-06-01 12:36:15 +0200

    feat(theme): add custom theme setup for Neovim
    - Introduced a new custom theme configuration in `custom/themes.lua`.
    - Added color overrides and custom highlights for various components.
    - Integrated with plugins like cmp, telescope, native_lsp, notify, and treesitter.
    - Introduced fallback to default colorscheme if `catppuccin-mocha` is not available.
    - Updated `themes.lua` to use the new custom theme setup and adjusted plugin configurations.

---
+++ .forge/nvim/lua/custom/themes.lua
@@ -0,0 +1,93 @@
+-- local c = require 'catppuccin.palettes.mocha'
+
+---@type CtpColors<string>
+local color_overrides = {
+  base = '#171717',
+  green = '#5eff6c',
+  red = '#FF6E5E',
+  yellow = '#f1ff5e',
+  blue = '#5ea1ff',
+  mauve = '#bd5eff',
+  sky = '#5ef1ff',
+}
+
+---@param colors CtpColors<string>
+local custom_highlights = function(colors)
+  return {
+    FloatBorder = { fg = colors.surface2, bg = colors.none },
+    CmpItemMenu = { style = { 'bold' } },
+    TelescopeBorder = {
+      fg = colors.surface2,
+      bg = colors.none,
+    },
+    TelescopeMatching = { fg = colors.yellow },
+    TelescopeNormal = {
+      bg = colors.none,
+    },
+    TelescopePromptBorder = {
+      fg = colors.surface2,
+      bg = colors.none,
+    },
+    TelescopePromptNormal = {
+      fg = colors.text,
+      bg = colors.none,
+    },
+    TelescopePromptPrefix = {
+      fg = colors.flamingo,
+      bg = colors.none,
+    },
+    TelescopePreviewTitle = {
+      fg = colors.base,
+      bg = colors.green,
+    },
+    TelescopePreviewNormal = {
+      bg = colors.base,
+    },
+    TelescopePreviewBorder = {
+      fg = colors.base,
+      bg = colors.base,
+    },
+    TelescopePromptTitle = {
+      fg = colors.base,
+      bg = colors.red,
+    },
+    TelescopeResultsTitle = {
+      fg = colors.mantle,
+      bg = colors.lavender,
+    },
+    TelescopeSelection = {
+      fg = colors.text,
+      bg = colors.base,
+      style = { 'bold' },
+    },
+    TelescopeSelectionCaret = { fg = colors.flamingo, style = { 'bold' } },
+  }
+end
+require('catppuccin').setup {
+  transparent_background = true,
+  color_overrides = {
+    all = color_overrides,
+  },
+  custom_highlights = function(colors)
+    return custom_highlights(colors)
+  end,
+  integrations = {
+    cmp = true,
+    fidget = true,
+    telescope = {
+      enabled = true,
+      -- style = 'nvchad',
+    },
+    native_lsp = {
+      enabled = true,
+    },
+    notify = true,
+    treesitter = true,
+  },
+  term_colors = true,
+}
+vim.o.termguicolors = true
+local ok, _ = pcall(vim.cmd.colorscheme, 'catppuccin-mocha')
+if not ok then
+  vim.cmd 'colorscheme default'
+end
--- .forge/nvim/lua/themes.lua
+++ .forge/nvim/lua/themes.lua
@@ -1,44 +1,18 @@
 return {
   {
-    'zaldih/themery.nvim',
-    opts = {
-      themes = { 'catppuccin-mocha', 'catppuccin-latte' },
-      themeConfigFile = '~/.config/nvim/lua/custom/themes.lua',
-      livePreview = true,
-    },
-    config = function(_, opts)
-      require('themery').setup(opts)
-      -- Themery block
-      -- This block will be replaced by Themery.
-      vim.o.termguicolors = true
-      vim.cmd 'colorscheme catppuccin-mocha'
-      vim.g.theme_id = 4
-      -- end themery block
-      vim.keymap.set('n', '<leader>ts', '<cmd>Themery<cr>', { noremap = true, desc = '[T]heme [S]witcher' })
-    end,
+    'rktjmp/lush.nvim',
+    event = 'VeryLazy',
+  },
+  {
+    'norcalli/nvim-colorizer.lua',
+    event = 'BufEnter',
+    opts = { '*' },
   },
   {
     'catppuccin/nvim',
     priority = 1000,
     config = function()
-      require('catppuccin').setup {
-        transparent_background = true,
-        integrations = {
-          cmp = true,
-          fidget = true,
-          telescope = {
-            enabled = true,
-            style = 'nvchad',
-          },
-          native_lsp = {
-            enabled = true,
-          },
-          neotree = true,
-          notify = true,
-          treesitter = true,
-        },
-        term_colors = true,
-      }
+      require 'custom.themes'
     end,
   },
   {
