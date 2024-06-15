2eeef342ee83a2b9535c42bad3dbc96cf87e1f36
hugovntr <26794193+hugovntr@users.noreply.github.com>
2024-06-15 16:07:57 +0200

    fix(nvim/cmp): Update key mappings for luasnip functionality
    - Change key mapping from <C-s> to <C-y> for expand_or_jumpable check
    - Update key mapping from <C-a> to <C-u> for locally_jumpable check

--- nvim/.config/nvim/lua/custom/cmp.lua
+++ nvim/.config/nvim/lua/custom/cmp.lua
@@ -8,12 +8,12 @@ luasnip.config.set_config {
 }
 require('luasnip.loaders.from_vscode').lazy_load()
 
-vim.keymap.set({ 'i', 'n', 's' }, '<C-s>', function()
+vim.keymap.set({ 'i', 'n', 's' }, '<C-y>', function()
   if luasnip.expand_or_jumpable() then
     luasnip.expand_or_jump()
   end
 end, { silent = true })
-vim.keymap.set({ 'i', 's' }, '<C-a>', function()
+vim.keymap.set({ 'i', 's' }, '<C-u>', function()
   if luasnip.locally_jumpable(-1) then
     luasnip.jump(-1)
   end
