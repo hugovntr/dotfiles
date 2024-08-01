0d7713ee0e3ccf742627a28eb4e7e2f410d585ca
hugovntr <26794193+hugovntr@users.noreply.github.com>
2024-08-01 10:28:53 +0200

    fix(wezterm/colors): Update split color and adjust bright color palette

--- wezterm/.config/wezterm/colors.lua
+++ wezterm/.config/wezterm/colors.lua
@@ -10,10 +10,10 @@ return {
 	selection_bg = "#3c4048",
 
 	scrollbar_thumb = "#16181a",
-	split = "#3c4048",
+	split = "rgb(0, 0, 0 / 0%)",
 
 	ansi = { "#16181a", "#ff6e5e", "#5eff6c", "#f1ff5e", "#5ea1ff", "#bd5eff", "#5ef1ff", "#ffffff" },
-	brights = { "#3c4048", "#ff6e5e", "#5eff6c", "#f1ff5e", "#5ea1ff", "#bd5eff", "#5ef1ff", "#ffffff" },
+	brights = { "#6b7280", "#ff6e5e", "#5eff6c", "#f1ff5e", "#5ea1ff", "#bd5eff", "#5ef1ff", "#ffffff" },
 	indexed = { [16] = "#ffbd5e", [17] = "#ff6e5e" },
 
 	tab_bar = {
