4c7b03b6525145bd965b64da2b16a1ea5275af85
hugovntr <26794193+hugovntr@users.noreply.github.com>
2024-08-01 10:28:57 +0200

    feat(sketchybar/music): Increase update frequency for music item to 30s

--- sketchybar/.config/sketchybar/items/music.sh
+++ sketchybar/.config/sketchybar/items/music.sh
@@ -7,5 +7,6 @@
 sketchybar -m --add item music right \
   --set music script="$PLUGIN_DIR/music.sh" \
   drawing=off \
+  update_freq=30 \
   label.padding_right=8 \
   --subscribe music media_change
