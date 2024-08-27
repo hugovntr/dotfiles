94506945257c169e2351b0b56245da85866ed784
hugovntr <26794193+hugovntr@users.noreply.github.com>
2024-08-27 05:07:20 +0200

    feat(wezterm/colors): Update color scheme and enhance theme structure
    - Refactor color definitions into a theme table for better organization.
    - Change background color from dark gray to a deeper shade for improved
      contrast.
    - Adjust tab bar background opacity for a more subtle appearance.
    - Maintain existing color values for ANSI and bright colors to ensure
      consistency.

--- wezterm/.config/wezterm/colors.lua
+++ wezterm/.config/wezterm/colors.lua
@@ -1,22 +1,35 @@
-return {
-	foreground = "#ffffff",
-	background = "#171717",
+local themes = {
+	custom = {
+		foreground = "#ffffff",
+		background = "#09090b",
 
-	cursor_bg = "#ffffff",
-	cursor_fg = "#000000",
-	cursor_border = "#ffffff",
+		cursor_bg = "#ffffff",
+		cursor_fg = "#000000",
+		cursor_border = "#ffffff",
 
-	selection_fg = "#ffffff",
-	selection_bg = "#3c4048",
+		selection_fg = "#ffffff",
+		selection_bg = "#3c4048",
 
-	scrollbar_thumb = "#16181a",
-	split = "rgb(0, 0, 0 / 0%)",
+		scrollbar_thumb = "#16181a",
+		split = "rgb(0, 0, 0 / 0%)",
 
-	ansi = { "#16181a", "#ff6e5e", "#5eff6c", "#f1ff5e", "#5ea1ff", "#bd5eff", "#5ef1ff", "#ffffff" },
-	brights = { "#6b7280", "#ff6e5e", "#5eff6c", "#f1ff5e", "#5ea1ff", "#bd5eff", "#5ef1ff", "#ffffff" },
-	indexed = { [16] = "#ffbd5e", [17] = "#ff6e5e" },
-
-	tab_bar = {
-		background = "rgb(22, 24, 26 / 20%)",
+		ansi = { "#16181a", "#ff6e5e", "#5eff6c", "#f1ff5e", "#5ea1ff", "#bd5eff", "#5ef1ff", "#ffffff" },
+		brights = { "#6b7280", "#ff6e5e", "#5eff6c", "#f1ff5e", "#5ea1ff", "#bd5eff", "#5ef1ff", "#ffffff" },
+		indexed = { [16] = "#ffbd5e", [17] = "#ff6e5e" },
 	},
 }
+
+local function merge(f, s)
+	for k, v in pairs(s) do
+		f[k] = v
+	end
+	return f
+end
+
+local M = merge(themes.custom, {
+	tab_bar = {
+		background = "rgb(9, 9, 11 / 95%)",
+	},
+})
+
+return M
