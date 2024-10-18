99c33a2e55c458bbd345001837108736a6b2cb1d
hugovntr <26794193+hugovntr@users.noreply.github.com>
2024-10-18 20:22:13 +0200

    feat(nvim/telescope): Implement shortcut for searching dotfiles
    - Added a new key mapping <leader>sd to search dotfiles.
    - The new functionality utilizes the Neovim configuration path
      to locate and search dotfiles, enhancing user experience in
      managing configuration files.
    - Removed the commented-out diagnostics search mapping for
      clarity and conciseness.

--- nvim/.config/nvim/lua/custom/telescope.lua
+++ nvim/.config/nvim/lua/custom/telescope.lua
@@ -46,7 +46,7 @@ vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles
 vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
 vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+-- vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
 -- vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
 vim.keymap.set('n', '<leader>sr', builtin.oldfiles, { desc = '[S]earch [R]ecent Files' })
 vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
@@ -77,3 +77,11 @@ end, { desc = '[S]earch [/] in Open Files' })
 vim.keymap.set('n', '<leader>sn', function()
   builtin.find_files { cwd = vim.fn.stdpath 'config' }
 end, { desc = '[S]earch [N]eovim files' })
+
+-- Shortcut for searching dotfiles
+vim.keymap.set('n', '<leader>sd', function()
+  local nvim_conf = vim.fn.stdpath 'config'
+  local resolve_dotfiles_path = io.popen('readlink -f ' .. nvim_conf, 'r'):read 'a+'
+  local dotfiles = resolve_dotfiles_path:match('(.*/nvim).*/nvim'):match '(.*)/nvim'
+  builtin.find_files { cwd = dotfiles }
+end, { desc = '[S]earch [D]otfiles' })
