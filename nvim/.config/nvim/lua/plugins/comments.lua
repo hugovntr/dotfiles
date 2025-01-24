49ea32fe9037304c93c5fb0516baf09de09e433a
hugovntr <26794193+hugovntr@users.noreply.github.com>
2025-01-24 20:56:54 +0100

    feat(nvim/plugins): Replace deprecated comment plugins with ts-comments
    Removed old comment plugins and replaced them with 'folke/ts-comments.nvim',
    which is now set to load on 'VeryLazy' event. Retained 'todo-comments.nvim'
    for managing TODOs and related comments with dependencies on plenary.nvim.

--- nvim/.config/nvim/lua/plugins/comments.lua
+++ nvim/.config/nvim/lua/plugins/comments.lua
@@ -1,21 +1,22 @@
 return {
-  {
-    'JoosepAlviste/nvim-ts-context-commentstring',
-    enable_autocmd = false,
-    event = 'InsertEnter',
-  },
-  {
-    'numToStr/Comment.nvim',
-    opts = {},
-    event = 'VeryLazy',
-    keys = {
-      { 'gcc', mode = 'n', desc = 'Comment toggle current line' },
-      { 'gc', mode = { 'n', 'o' }, desc = 'Comment toggle linewise' },
-      { 'gc', mode = 'x', desc = 'Comment toggle linewise (visual)' },
-      { 'gbc', mode = 'n', desc = 'Comment toggle current block' },
-      { 'gb', mode = { 'n', 'o' }, desc = 'Comment toggle blockwise' },
-      { 'gb', mode = 'x', desc = 'Comment toggle blockwise (visual)' },
-    },
-  },
+  -- {
+  --   'JoosepAlviste/nvim-ts-context-commentstring',
+  --   enable_autocmd = false,
+  --   event = 'InsertEnter',
+  -- },
+  -- {
+  --   'numToStr/Comment.nvim',
+  --   opts = {},
+  --   event = 'VeryLazy',
+  --   keys = {
+  --     { 'gcc', mode = 'n', desc = 'Comment toggle current line' },
+  --     { 'gc', mode = { 'n', 'o' }, desc = 'Comment toggle linewise' },
+  --     { 'gc', mode = 'x', desc = 'Comment toggle linewise (visual)' },
+  --     { 'gbc', mode = 'n', desc = 'Comment toggle current block' },
+  --     { 'gb', mode = { 'n', 'o' }, desc = 'Comment toggle blockwise' },
+  --     { 'gb', mode = 'x', desc = 'Comment toggle blockwise (visual)' },
+  --   },
+  -- },
+  { 'folke/ts-comments.nvim', event = 'VeryLazy', opts = {} },
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 }
