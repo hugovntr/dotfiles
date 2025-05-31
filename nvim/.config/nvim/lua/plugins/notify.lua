0706a648bff7e44f224a3c565e7354b32fafa866
hugovntr <26794193+hugovntr@users.noreply.github.com>
2025-05-31 13:47:54 +0200

    fix(nvim/plugins/notify): disable lsp-notify plugin and filter specific notify errors
    Comment out the 'mrded/nvim-lsp-notify' plugin to disable it.
    Add a route to 'folke/noice.nvim' to suppress notifications for the
    'Request textDocument/inlayHint failed' error, preventing unnecessary
    error messages from appearing.

--- nvim/.config/nvim/lua/plugins/notify.lua
+++ nvim/.config/nvim/lua/plugins/notify.lua
@@ -3,14 +3,14 @@ return {
     'rcarriga/nvim-notify',
     opts = {},
   },
-  {
-    'mrded/nvim-lsp-notify',
-    config = function()
-      require('lsp-notify').setup {
-        notify = require 'notify',
-      }
-    end,
-  },
+  -- {
+  --   'mrded/nvim-lsp-notify',
+  --   config = function()
+  --     require('lsp-notify').setup {
+  --       notify = require 'notify',
+  --     }
+  --   end,
+  -- },
   {
     'folke/noice.nvim',
     event = 'VeryLazy',
@@ -28,6 +28,15 @@ return {
           silent = true,
           view = nil,
           opts = {},
+        },
+      },
+      routes = {
+        {
+          filter = {
+            event = 'notify',
+            find = 'Request textDocument/inlayHint failed',
+          },
+          opts = { skip = true },
         },
       },
     },
