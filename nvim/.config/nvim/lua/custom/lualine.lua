3e0aea5c9c973a1a35f8135f58815c6343f24ad3
hugovntr <26794193+hugovntr@users.noreply.github.com>
2025-06-06 11:14:38 +0200

    refactor(nvim/lualine): replace nvim-web-devicons with mini.icons and adjust config
    - Removed 'neo-tree' extension from custom lualine setup
    - Replaced 'nvim-tree/nvim-web-devicons' with 'echasnovski/mini.icons' plugin
    - Initialized mini.icons and enabled mock nvim-web-devicons in lualine config
    - Disabled navic attachment and autocmd creation in barbecue.nvim dependency

--- nvim/.config/nvim/lua/custom/lualine.lua
+++ nvim/.config/nvim/lua/custom/lualine.lua
@@ -166,7 +166,4 @@ require('lualine').setup {
     lualine_y = {},
     lualine_z = { 'location' },
   },
-  extensions = {
-    'neo-tree',
-  },
 }
--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -2,17 +2,22 @@ return {
   'nvim-lualine/lualine.nvim',
   event = 'VimEnter',
   dependencies = {
-    'nvim-tree/nvim-web-devicons',
+    { 'echasnovski/mini.icons', opts = {} },
     {
       'utilyre/barbecue.nvim',
       dependencies = {
         'SmiteshP/nvim-navic',
       },
-      opts = {},
+      opts = {
+        attach_navic = false,
+        create_autocmd = false,
+      },
     },
     'folke/noice.nvim',
   },
   config = function()
+    require('mini.icons').setup()
+    require('mini.icons').mock_nvim_web_devicons()
     require 'custom.lualine'
   end,
 }
