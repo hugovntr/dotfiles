b5ab788b0ac6774f7aa110b2f9fc25a13f0cece3
hugovntr <26794193+hugovntr@users.noreply.github.com>
2025-06-12 10:53:46 +0200

    chore(fish/config): remove unused shell initialization and custom scripts
    Deleted initialization for starship, zoxide, and fnm, as well as sourcing
    of vim-motion, aliases, and abbreviations. Simplified config by removing
    redundant interactive shell checks and custom function imports.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,9 +12,6 @@ set -gx EDITOR nvim
 # Path
 fish_add_path --append --path $HOME/bin $HOME/.local/bin
 
-# Vim motion
-source $XDG_CONFIG_HOME/fish/functions/vim-motion.fish
-
 # Homebrew
 if test -f /opt/homebrew/bin/brew 
   set -gx HOMEBREW_PREFIX "/opt/homebrew"
@@ -22,23 +19,6 @@ if test -f /opt/homebrew/bin/brew
   set -gx HOMEBREW_REPOSITORY "/opt/homebrew"
   fish_add_path --append --path /opt/homebrew/bin /opt/homebrew/sbin
 end
-
-# Initialize starship
-set -gx STARSHIP_CONFIG $XDG_CONFIG_HOME/starship.toml
-starship init fish | source
-
-# Initialize zoxide
-zoxide init --cmd cd fish | source
-
-# FNM
-fnm env --use-on-cd --shell fish | source
-
-if status is-interactive
-  # Check if we're in interactive shell
-end
-
-source $XDG_CONFIG_HOME/fish/aliases.fish
-source $XDG_CONFIG_HOME/fish/abbr.fish
 
 # Source colors
 if test -f $XDG_CONFIG_HOME/colors/colors.sh
