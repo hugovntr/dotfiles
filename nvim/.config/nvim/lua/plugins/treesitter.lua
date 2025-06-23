fdf9c49ebca1c2d36d6e89ea4658c411354b8ab0
hugovntr <26794193+hugovntr@users.noreply.github.com>
2025-06-23 10:40:52 +0200

    fix(nvim/treesitter): update fold settings for better folding behavior
    Change foldmethod to use vim.opt and switch foldexpr to the native
    nvim_treesitter fold expression. Add foldtext customization and set a
    high initial foldlevel. Disable folding by default with 'nofoldenable'.

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -87,8 +87,11 @@ return {
       vim.keymap.set({ 'n', 'x', 'o' }, ',', r.repeat_last_move_opposite)
 
       -- folds
-      vim.wo.foldmethod = 'expr'
-      vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+      vim.opt.foldmethod = 'expr'
+      -- vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+      vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+      vim.opt.foldtext = 'v:lua.vim.treesitter.foldtext()'
+      vim.opt.foldlevel = 44
       vim.cmd [[ set nofoldenable ]]
 
       -- make f, F, t, T also repeatable
