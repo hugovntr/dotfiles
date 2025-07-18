477ce64eac47754924d9920d22f414af3bbecb64
hugovntr <26794193+hugovntr@users.noreply.github.com>
2025-07-18 15:13:40 +0200

    feat(nvim/plugins): optimize which-key loading with deferred operator handling
    - Change loading event from 'VimEnter' to 'VeryLazy' for better startup performance
    - Add defer function to delay keybinding display when using certain operators or visual modes

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -1,5 +1,12 @@
 return { -- Useful plugin to show you pending keybinds.
   'folke/which-key.nvim',
-  event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-  opts = {},
+  event = 'VeryLazy',
+  opts = {
+    defer = function(ctx)
+      if vim.list_contains({ 'd', 'y' }, ctx.operator) then
+        return true
+      end
+      return vim.list_contains({ '<C-V>', 'V' }, ctx.mode)
+    end,
+  },
 }
