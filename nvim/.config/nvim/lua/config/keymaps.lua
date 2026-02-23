a3352fc272b089f428551cff393c15ec4ba74096
hugovntr <26794193+hugovntr@users.noreply.github.com>
2026-02-23 14:36:17 +0100

    refactor(nvim/keymaps): change <leader>q to close window instead of buffer
    - Switch from bd (delete buffer) to close to preserve buffer while closing window
    - Update description to clarify behavior: "Close current window but keep the buffer open"
    - Maintain remapping and silent flag settings for consistency

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -61,7 +61,7 @@ nnoremap('cw', '"_ciw') -- Change inside word without copy
 -- Buffer management
 nnoremap('<C-p>', '<cmd>bprev<cr>', { desc = 'Previous buffer' })
 nnoremap('<C-n>', '<cmd>bnext<cr>', { desc = 'Next buffer' })
-nnoremap('<leader>q', '<cmd>bd<cr>', { silent = false, desc = 'Close current buffer', remap = true })
+nnoremap('<leader>q', '<cmd>close<cr>', { silent = false, desc = 'Close current window but keep the buffer open', remap = true })
 -- nnoremap('qq', '<cmd>bd<cr>', { silent = true, desc = 'Close current buffer' })
 
 -- One hand fast travel
