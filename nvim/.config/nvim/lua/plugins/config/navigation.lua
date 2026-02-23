badced20b3fb2e968051ab06e53701a157f6ec1c
hugovntr <26794193+hugovntr@users.noreply.github.com>
2026-02-23 14:47:50 +0100

    feat(nvim/navigation): add keymaps for resizing window splits
    - Bind <C-M-j>, <C-M-k>, <C-M-l>, and <C-M-m> to resize splits left, up, down, and right respectively
    - Maintains consistency with existing directional keybindings (<C-j>, <C-k>, <C-l>)

--- nvim/.config/nvim/lua/plugins/config/navigation.lua
+++ nvim/.config/nvim/lua/plugins/config/navigation.lua
@@ -5,6 +5,12 @@ vim.keymap.set('n', '<C-j>', splits.move_cursor_left)
 vim.keymap.set('n', '<C-k>', splits.move_cursor_up)
 vim.keymap.set('n', '<C-l>', splits.move_cursor_down)
 
+-- Resize splits
+vim.keymap.set('n', '<C-M-j>', splits.resize_left)
+vim.keymap.set('n', '<C-M-k>', splits.resize_up)
+vim.keymap.set('n', '<C-M-l>', splits.resize_down)
+vim.keymap.set('n', '<C-M-m>', splits.resize_right)
+
 -- Treewalker
 vim.keymap.set('n', '¬', '<cmd>Treewalker Down<cr>') -- down
 vim.keymap.set('n', 'ø', '<cmd>Treewalker Up<cr>') -- up
