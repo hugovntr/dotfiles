f1e9b4a89a8938c91638352f8f4c6dd21a07f05f
hugovntr <26794193+hugovntr@users.noreply.github.com>
2026-05-29 14:16:12 +0200

    feat(nvim/plugins): add keymap for Spectre search and replace

--- nvim/.config/nvim/lua/plugins/code.lua
+++ nvim/.config/nvim/lua/plugins/code.lua
@@ -126,6 +126,7 @@ return {
     event = 'VeryLazy',
     config = function()
       require 'plugins.config.spectre'
+      vim.keymap.set('n', '<leader>sp', '<cmd>Spectre<cr>', { desc = '[Sp]ectre', silent = true })
     end,
   },
 
