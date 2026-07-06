72858ec856be76cd0afcb35c8031331aa2772330
hugovntr <26794193+hugovntr@users.noreply.github.com>
2026-07-06 14:32:58 +0200

    feat(nvim/lsp): configure ltex-utils with custom dictionary paths
    - Move ltex-utils.nvim from LSP dependencies to a standalone plugin configuration.
    - Define custom dictionary paths using the Neovim cache directory.
    - Set the backend to ltex_plus.

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -27,10 +27,23 @@ return {
     event = 'VeryLazy',
     dependencies = {
       'mason-org/mason.nvim',
-      'jhofscheier/ltex-utils.nvim', -- LaTeX LSP method implementations
     },
     config = function()
       require 'plugins.config.lsp'
     end,
+  },
+  {
+    'jhofscheier/ltex-utils.nvim', -- LaTeX LSP method implementations
+    opts = {
+      dictionary = {
+        path = vim.api.nvim_call_function('stdpath', { 'cache' }) .. '/ltex/',
+        filename = function(lang)
+          return lang .. '.txt'
+        end,
+        use_vim_dict = false,
+        vim_cmd_output = false,
+      },
+      backend = 'ltex_plus',
+    },
   },
 }
